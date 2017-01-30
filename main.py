074fd7b2e803aa2ccec85c237aedc4221c8a9865
Greg Stead <gregstead@gmail.com>
2017-01-30 16:37:45 -0600

    Added form and submit button

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ import string
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = "Hello world!"
-        encrypted_message = caesar.encrypt("Hello world",13)
-
-        self.response.write(encrypted_message)
+        encrypted_message = caesar.encrypt(message,13)
+        encrypt_textarea = "<textarea>" + encrypted_message + "</textarea>"
+        submit = '''<input type="submit" />'''
+        form = "<form>" + encrypt_textarea + "<br>" + submit + "</form>"
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
